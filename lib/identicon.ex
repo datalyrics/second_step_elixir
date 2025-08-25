a14099c49d8f37fbaaf2b4555218680d2f6e9d65
datalyrics <info@datalyrics.de>
2025-08-25 19:09:13 +0200

    use struct in has_input method

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -6,9 +6,10 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    :crypto.hash(:md5, input)
+    hex = :crypto.hash(:md5, input)
     |> :binary.bin_to_list
-  end
 
+    %Identicon.Image{hex: hex}
+  end
 
 end
