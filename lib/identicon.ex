8691d38a5b9c928fb9576a2d0e4553a507696ec7
datalyrics <info@datalyrics.de>
2025-08-25 21:05:54 +0200

    add the mirror function

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,7 +3,21 @@ defmodule Identicon do
     input
     |> hash_input
     |> pic_color
+    |> build_grid
   end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&mirror_row/1)
+
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
+  end
+
 
   def pic_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
